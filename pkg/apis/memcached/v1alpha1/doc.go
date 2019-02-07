afb573b5236e9a3d1f0a4aaefc070d89e803a0fb
gkarthiks <github.gkarthiks@gmail.com>
2019-02-06 20:28:29 -0800

    add(): adding addition files

---
+++ pkg/apis/memcached/v1alpha1/doc.go
@@ -0,0 +1,4 @@
+// Package v1alpha1 contains API Schema definitions for the memcached v1alpha1 API group
+// +k8s:deepcopy-gen=package,register
+// +groupName=memcached.gkarthiks.io
+package v1alpha1
